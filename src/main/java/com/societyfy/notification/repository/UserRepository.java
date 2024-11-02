29aaa70c82ab6e5a4cc89c0dd6df8376b6fbf3f2
Kushal-Dalasaniya <kushaldalasaniya2930@gmail.com>
2024-11-02 20:52:37 +0530

    Remove unused code

--- src/main/java/com/societyfy/notification/repository/UserRepository.java
+++ src/main/java/com/societyfy/notification/repository/UserRepository.java
@@ -7,8 +7,4 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    @Query("select u from User u " +
-            "left join UserRole ur on ur.id=u.roleId " +
-            "where ur.role = 'ADMIN'")
-    User findAdmin();
 }
